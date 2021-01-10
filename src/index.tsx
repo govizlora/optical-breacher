b5315e4ef520e096faf6a33fcd671b3357d8b7be
govizlora <govizlora@gmail.com>
2021-01-09 16:11:28 -0800

    Update location

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import './main.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
     // .then((registration) => {
     //   console.log('SW registered: ', registration)
     // })
